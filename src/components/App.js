7986a2439d281481eba0d1e851131024fb3d397a
Kosta <k.v.dinkov@gmail.com>
2018-04-20 15:10:44 +0300

    refactor: migrated to create-react-app project and rails style file project organization

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import ConnectedTodos from './todos';
+import ConnectedGoals from './goals';
+import {handleInitialData} from '../actions/shared';
+
+class App extends React.Component {
+  componentDidMount() {
+    const {dispatch} = this.props;
+    dispatch(handleInitialData());
+  }
+  render() {
+    if (this.props.loading) {
+      return (
+        <h3>Loading</h3>
+      );
+    }
+    return (
+      <div>
+        <ConnectedTodos/>
+        <ConnectedGoals/>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  (state) => (
+    { loading: state.loading }
+  ))(App);
